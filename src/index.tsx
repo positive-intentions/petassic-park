252bfe8c6a03ee2554e80be3fbf740a969804a91
xoron <rmaher84@gmail.com>
2022-05-27 16:50:17 +0000

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame(() => {
+    ref.current.rotation.x += 0.01; //eslint-disable-line
+  });
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props} //eslint-disable-line
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
+
+ReactDOM.render(
+  <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>,
+  document.getElementById('app-root'),
+);
